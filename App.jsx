14fe20139eeeb1d98f55354aede0e40b8351bad5
SiddhantChadha <sid.01chadha@gmail.com>
2023-04-08 23:29:21 +0530

    added signup screen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import LoginScreen from './src/screens/LoginScreen'
+import SignupScreen from './src/screens/SignupScreen'
 
 const App = () => {
   return (
-    <LoginScreen></LoginScreen>
+    <SignupScreen></SignupScreen>
   )
 }
 
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -8,11 +8,13 @@ import { ChevronLeftIcon } from "react-native-heroicons/outline";
 const LoginScreen = () => {
     return (
         <ScrollView showsVerticalScrollIndicator={false}>
+            
             <View style={{ marginVertical: 10, flexDirection: "row", justifyContent: "space-between"}}>
                 <ChevronLeftIcon color={Colors.BLACK} style={{}}></ChevronLeftIcon>
                 <Text style={{ color: Colors.BLACK, fontSize: 16, fontWeight: "600" }}>Login</Text>
                 <View></View>
             </View>
+
             <Text style={{ color: Colors.GREY_4A, marginHorizontal: '10%' }}>Type in your Email ID and Password and click Go to Feed</Text>
             <InputBox placeholder="Email"></InputBox>
             <InputBox placeholder="Password" secureTextEntry={true}></InputBox>
