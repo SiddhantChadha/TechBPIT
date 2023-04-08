a3f92f97777a26ae4d67cb6e7a0fbec80f4beed9
SiddhantChadha <sid.01chadha@gmail.com>
2023-04-08 23:17:15 +0530

    added login screen toolbar

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -29,6 +29,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CustomButton;
-
 export default CustomButton
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import InputBox from '../components/InputBox'
 import CustomButton from '../components/CustomButton'
 import { Colors } from '../color'
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
 
 const LoginScreen = () => {
     return (
         <ScrollView showsVerticalScrollIndicator={false}>
-            <View >
-                <Text style={{ color: Colors.BLACK, fontSize: 16 ,textAlign:"center", marginVertical:10,fontWeight:"600"}}>Login</Text>
+            <View style={{ marginVertical: 10, flexDirection: "row", justifyContent: "space-between"}}>
+                <ChevronLeftIcon color={Colors.BLACK} style={{}}></ChevronLeftIcon>
+                <Text style={{ color: Colors.BLACK, fontSize: 16, fontWeight: "600" }}>Login</Text>
+                <View></View>
             </View>
             <Text style={{ color: Colors.GREY_4A, marginHorizontal: '10%' }}>Type in your Email ID and Password and click Go to Feed</Text>
             <InputBox placeholder="Email"></InputBox>
