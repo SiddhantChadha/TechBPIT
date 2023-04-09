a2c596c8ae9529e910c3263820ed05aabbe65f16
Tushar Jain <tjain210@gmail.com>
2023-04-09 15:58:50 +0530

    Formmatted code.

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,33 +1,33 @@
-import {Pressable, StyleSheet, Text} from "react-native";
-import React from "react";
-import {Colors} from "../colors";
+import {Pressable, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import {Colors} from '../colors';
 
-const CustomButton = (props) => {
-    return (
-        <Pressable style={styles.button} onPress={props.onPress}>
-            <Text style={styles.text}>{props.title}</Text>
-        </Pressable>
-    );
+const CustomButton = props => {
+  return (
+    <Pressable style={styles.button} onPress={props.onPress}>
+      <Text style={styles.text}>{props.title}</Text>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 16,
-        paddingHorizontal: 32,
-        marginHorizontal: "10%",
-        borderRadius: 8,
-        elevation: 3,
-        backgroundColor: Colors.PRIMARY_BLUE,
-        marginVertical: '5%'
-    },
-    text: {
-        fontSize: 16,
-        fontWeight: 500,
-        letterSpacing: 0.25,
-        color: Colors.WHITE,
-    },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    marginHorizontal: '10%',
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: Colors.PRIMARY_BLUE,
+    marginVertical: '5%',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 500,
+    letterSpacing: 0.25,
+    color: Colors.WHITE,
+  },
 });
 
-export default CustomButton
+export default CustomButton;
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,23 +1,23 @@
-import {TextInput} from 'react-native'
-import React, {useState} from 'react'
+import {TextInput} from 'react-native';
+import React, {useState} from 'react';
 
-const InputBox = (props) => {
+const InputBox = props => {
+  const [data, setData] = useState('');
 
-    const [data, setData] = useState("");
+  return (
+    <TextInput
+      value={data}
+      onChangeText={setData}
+      style={{
+        marginHorizontal: '10%',
+        marginVertical: '5%',
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingHorizontal: '4%',
+      }}
+      placeholder={props.placeholder}
+      secureTextEntry={props.secureTextEntry}/>
+  );
+};
 
-
-    return (
-        <TextInput value={data} onChangeText={setData}
-                   style={{
-                       marginHorizontal: '10%',
-                       marginVertical: '5%',
-                       borderWidth: 1,
-                       borderRadius: 10,
-                       paddingHorizontal: '4%'
-                   }}
-                   placeholder={props.placeholder} secureTextEntry={props.secureTextEntry}>
-        </TextInput>
-    )
-}
-
-export default InputBox
+export default InputBox;
