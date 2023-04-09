03db40b9d12a59a02869e017ee62602246ba8476
Tushar Jain <tjain210@gmail.com>
2023-04-09 17:37:07 +0530

    Added community post item and resource item ui.

---
+++ src/components/ResourceItem.jsx
@@ -0,0 +1,71 @@
+import {Image, View, Text} from 'react-native';
+import React from 'react';
+import {ChevronDownIcon} from 'react-native-heroicons/outline';
+import {Colors} from '../colors';
+
+const ResourceItem = () => {
+  return (
+    <View
+      style={{
+        borderRadius: 20,
+        marginVertical: 12,
+        marginHorizontal: '5%',
+        elevation: 3,
+        backgroundColor: Colors.WHITE,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginTop: 16,
+          marginLeft: '5%',
+        }}>
+        <Image
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
+          }}
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 100,
+          }}
+          resizeMode="center"
+        />
+        <View
+          style={{
+            flexShrink: 1,
+            justifyContent: 'center',
+            marginHorizontal: '3%',
+          }}>
+          <Text style={{fontSize: 16, fontWeight: 500, color: Colors.BLACK}}>
+            A new resource was added to Node Js community
+          </Text>
+          <Text style={{fontSize: 10, color: Colors.GREY_70}}>
+            15 mins read time
+          </Text>
+        </View>
+        <ChevronDownIcon
+          color={Colors.BLACK}
+          style={{marginRight: '3%', alignSelf: 'flex-start'}}
+        />
+      </View>
+      <Image
+        source={{
+          uri: 'https://static.toiimg.com/photo/msid-91006178/91006178.jpg',
+        }}
+        style={{
+          width: '100%',
+          aspectRatio: '1',
+          marginTop: 8,
+          alignSelf: 'center',
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+        }}
+        resizeMethod="scale"
+      />
+    </View>
+  );
+};
+
+export default ResourceItem;
