f8181b07e346e5f8c85de8af736792f8b777a3bd
SiddhantChadha <sid.01chadha@gmail.com>
2023-04-09 20:16:57 +0530

    added event detail page

--- App.jsx
+++ App.jsx
@@ -7,22 +7,23 @@ import OTPScreen from './src/screens/OTPScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import EventItem from './src/components/EventItem';
 import EventPostItem from './src/components/EventPostItem';
+import PostDetailsScreen from './src/screens/PostDetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator
-    //     initialRouteName="Welcome"
-    //     screenOptions={{headerShown: false}}>
-    //     <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    //     <Stack.Screen name="Login" component={LoginScreen} />
-    //     <Stack.Screen name="Otp" component={OTPScreen} />
-    //     <Stack.Screen name="Signup" component={SignupScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <EventPostItem></EventPostItem>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Otp" component={OTPScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 };
 
