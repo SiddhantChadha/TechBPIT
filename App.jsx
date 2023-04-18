4ba29dd1fafe7403b39feec6621da28bf04961a6
Tushar Jain <tjain210@gmail.com>
2023-04-18 19:17:55 +0530

    added bottom nav bar

--- App.jsx
+++ App.jsx
@@ -8,9 +8,18 @@ import SignupScreen from './src/screens/SignupScreen';
 import EventItem from './src/components/EventItem';
 import EventPostItem from './src/components/EventPostItem';
 import PostDetailsScreen from './src/screens/PostDetailsScreen';
-import HomeScreen from './src/screens/HomeScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  ChevronLeftIcon,
+  UserIcon,
+  HomeIcon,
+  CalendarDaysIcon,
+  MagnifyingGlassIcon,
+} from 'react-native-heroicons/outline';
+import {Colors} from './src/colors';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
@@ -24,8 +33,52 @@ const App = () => {
     //     <Stack.Screen name="Signup" component={SignupScreen} />
     //   </Stack.Navigator>
     // </NavigationContainer>
-    <HomeScreen/>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarInactiveTintColor: Colors.BLACK,
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: () => {
+              return <HomeIcon color={Colors.BLACK} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Explore"
+          component={SignupScreen}
+          options={{
+            tabBarIcon: () => {
+              return <MagnifyingGlassIcon color={Colors.BLACK} />;
+            },
+          }}
+        />
 
+        <Tab.Screen
+          name="Events"
+          component={OTPScreen}
+          options={{
+            tabBarIcon: () => {
+              return <CalendarDaysIcon color={Colors.BLACK} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={OTPScreen}
+          options={{
+            tabBarActiveTintColor: 'blue',
+            tabBarIcon: ({focused, color, size}) => {
+              return <UserIcon color={color} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
