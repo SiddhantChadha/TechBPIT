71b51fdbaaf58246e0921c62069c512e48f5a555
SiddhantChadha <sid.01chadha@gmail.com>
2023-05-27 17:33:59 +0530

    added recent personal

--- src/components/ChatThreadCard.jsx
+++ src/components/ChatThreadCard.jsx
@@ -1,10 +1,25 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const ChatThreadCard = () => {
+const ChatThreadCard = ({obj}) => {
   return (
-    <View>
-      <Text>ChatThreadCard</Text>
+    <View className="flex flex-row bg-white p-3">
+      <View className="">
+        <Image
+          source={{
+            uri: obj.image,
+          }}
+          className="rounded-full h-14 w-14 bg-black"
+        />
+      </View>
+
+      <View className="flex">
+        <Text className="text-black text-lg">{obj.username}</Text>
+        <Text>You:Image</Text>
+      </View>
+      <View className="self-end">
+        <Text className="text-GRAY_70 text-md flex-1">{obj.lastMessage.timestamp}</Text>
+      </View>
     </View>
   );
 };
