1cd9647d7214278f95eecb82ade0d1a7f2483c76
SiddhantChadha <sid.01chadha@gmail.com>
2023-06-10 18:55:32 +0530

    added modal

--- src/components/DateInputBox.jsx
+++ src/components/DateInputBox.jsx
@@ -1,8 +1,9 @@
-import {TextInput} from 'react-native';
+import {TextInput, Modal} from 'react-native';
 import React, {useState, forwardRef, useImperativeHandle} from 'react';
 
 const DateInputBox = forwardRef((props, ref) => {
   const [data, setData] = useState(props.data);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useImperativeHandle(
     ref,
@@ -15,20 +16,39 @@ const DateInputBox = forwardRef((props, ref) => {
   );
 
   return (
-    <TextInput
-      value={data}
-      onChangeText={setData}
-      style={{
-        marginLeft:props.marginLeft,
-        marginRight:props.marginRight,
-        marginVertical: '3%',
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: '4%',
-        width:150
-      }}
-      placeholder={props.placeholder}
-    />
+    <>
+      <TextInput
+        value={data}
+        onChangeText={setData}
+        style={{
+          marginLeft: props.marginLeft,
+          marginRight: props.marginRight,
+          marginVertical: '3%',
+          borderWidth: 1,
+          borderRadius: 10,
+          paddingHorizontal: '4%',
+          width: 150,
+        }}
+        placeholder={props.placeholder}
+      />
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setModalVisible(false)}>
+        <View className="bg-[rgba(0,0,0,0.5)] flex-1 justify-center items-center">
+          <View className="bg-white ">
+            {/* <CalendarPicker
+                startFromMonday={true}
+                allowRangeSelection={false}
+                todayBackgroundColor="#f2e6ff"
+                selectedDayColor="#7300e6"
+                selectedDayTextColor="#FFFFFF"
+              /> */}
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 });
 
